4bb872f52205fa08f214d2b312fbeec6597c38d7
laislealdina <laisdina@id.uff.br>
2018-08-13 13:13:33 -0300

    Aula 35 - Paginacao com parametros opcionais na requisicao.

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -61,8 +61,14 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception{
 		Categoria cat1 = new Categoria(null,"Informática");
-		Categoria cat2 = new Categoria (null,"Impressora");
-	
+		Categoria cat2 = new Categoria (null,"Impressora");		
+		Categoria cat3 = new Categoria(null,"Cama, mesa e banho");
+		Categoria cat4 = new Categoria (null,"Maquiagem");
+		Categoria cat5 = new Categoria(null,"Acessórios");
+		Categoria cat6 = new Categoria (null,"Moda");
+		Categoria cat7 = new Categoria(null,"Perfumes");
+		Categoria cat8 = new Categoria (null,"Sapatos");
+		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null, "Impressora",800.00);
 		Produto p3 = new Produto(null, "mouse",80.00);
@@ -74,7 +80,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
@@ -138,5 +144,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p3.getItens().addAll(Arrays.asList(ip2));
 		
 		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
+		
 	}
 }
